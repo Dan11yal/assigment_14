0be216614e362802c054a4c18c6c61e156e65141
Dan11yal <220107072@stu.sdu.edu.kz>
2023-12-12 20:45:21 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <game-board></game-board>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameBoard from './components/GameBoard.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    GameBoard
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,html{
+  background: rgb(191,16,137);
+  background: linear-gradient(126deg, rgba(191,16,137,1) 21%, rgba(255,14,88,1) 43%, rgba(14,31,255,1) 69%, rgba(118,223,242,1) 87%);
+  background-repeat: no-repeat;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  min-height: 100%;
+}
+.app{
+  display: flex;
+justify-content: center;
+   background-color: transparent;
+
 }
 </style>
